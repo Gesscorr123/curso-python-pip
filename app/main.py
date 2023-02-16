b69b59d6b5f42f897d43b22f2cfd1af0f5b7475f
root <root@DESKTOP-ESBE8BN>
2023-02-15 21:58:20 -0500

    Echo un cambio en main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-import utils # al importarlo podemos comenzar a ejecutarlo
+"""import utils # al importarlo podemos comenzar a ejecutarlo
 import read_csv
 import charts
 
@@ -28,4 +28,29 @@ def run() :
 # Ejecutarllo como scrip y como módulo
 if __name__ == '__main__' : 
   #run()
+  run()
+  """
+  import utils
+import read_csv
+import charts
+
+def run():
+  data = read_csv.read_csv('./app/data.csv')
+  data = list(filter(lambda item : item['Continent'] == 'South America',data))
+
+  countries = list(map(lambda x: x['Country'], data))
+  percentages = list(map(lambda x: x['World Population Percentage'], data))
+  charts.generate_pie_chart(countries, percentages)
+  '''
+  country = input('Type Country => ')
+
+  result = utils.population_by_country(data, country)
+
+  if len(result) > 0:
+    country = result[0]
+    labels, values = utils.get_population(country)
+    charts.generate_bar_chart(labels, values)
+  '''
+
+if __name__ == '__main__':
   run()
